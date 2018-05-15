3e3702c5aad408d5db93f11c40072913218a30b1
alvxyh <alija.levic@vxyh.se>
2018-05-15 06:59:37 +0000

    add script

---
+++ runapp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+imageName alija:nodejsapp
+containerName=nodecontainer
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p 5000:5000 --name $containerName $imageName
